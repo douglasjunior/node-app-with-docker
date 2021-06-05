5112bbf0b78ca9baa6bb13b944f46e1a1d9e5ddc
Douglas Nassif Roma Junior <douglasjunior@users.noreply.github.com>
2021-06-04 22:11:00 -0300

    Update app.js

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -2,6 +2,7 @@ function App() {
     const { Container, Row, Col } = ReactBootstrap;
     return (
         <Container>
+            <h1>Hello world!</h1>
             <Row>
                 <Col md={{ offset: 3, span: 6 }}>
                     <TodoListCard />
